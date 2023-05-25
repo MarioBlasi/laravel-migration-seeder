581d21c4c4625339038f76779c49754ded731846
MarioBlasi <mblasi509@gmail.com>
2023-05-25 16:15:44 +0100

    Schema::create('trains', function (Blueprint $table)

--- laravel-migration-seeder/database/migrations/2023_05_25_145931_create_trains_table.php
+++ laravel-migration-seeder/database/migrations/2023_05_25_145931_create_trains_table.php
@@ -14,9 +14,21 @@ return new class extends Migration
     public function up()
     {
         Schema::create('trains', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('azienda')->nullable();
+            $table->string('stazione_partenza')->nullable();
+            $table->string('stazione_arrivo')->nullable();
+            $table->time('orario_partenza')->nullable();
+            $table->time('orario_arrivo')->nullable();
+            $table->string('codice_treno')->nullable();
+            $table->integer('numero_carrozze')->unsigned()->nullable();
+            $table->boolean('in_orario')->default(true);
+            $table->boolean('cancellato')->default(false);
             $table->timestamps();
         });
+        
+        
+        
     }
 
     /**
